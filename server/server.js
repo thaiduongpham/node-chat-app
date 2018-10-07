3fbde1c522e46e9b8220a675062cebecbc5a79d7
thaiduongpham <thaiduongpham@outlook.com>
2018-10-07 22:26:04 +0200

    Add Users class and test cases

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 
 const publicPath = path.join(__dirname, '../public');
 var port = process.env.PORT || 3000; // setup for heroku as prod env
@@ -14,9 +15,23 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
   
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to Chat App!'));
+  
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name & room name is required.');
+    }
+  
+    socket.join(params.room);
+    // socket.leave('The Office Fans');
+    // io.emit -> io.to('The Office Fans').emit
+    // socket.broadcast.emit -> socket.to('The Office Fans').emit
+    // socket.emit -> only individual User
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined!'));
+    socket.to(params.room).emit('newMessage', generateMessage('Admin', 'Welcome to Chat App!'));  
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     console.log('newMessage: ', message);
